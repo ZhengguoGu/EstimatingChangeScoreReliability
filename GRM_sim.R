c35171397e48efe56dea5296433a4a97ea735ed7
Zhengguo Gu <z.gu@uvt.nl>
2016-09-29 22:18:30 +0200

    Update GRM_sim.R

--- GRM_sim.R
+++ GRM_sim.R
@@ -1,22 +1,21 @@
 # this is a program for simulating item reponse data from the graded response model (Samejima, 1969)
 
 GRM_sim <- function(ability, itempar){
-  
+
   n_sub <- length(ability)
   response <- matrix(NA, n_sub, nrow(itempar))
-  
-  compare_sub <- runif(n_sub, min=0, max=1)
-  
+
+
   for(i in 1:n_sub){
-    
-    
+
+
     numeritor <- exp(sweep((ability[i]-itempar[, -1]), 1, itempar[, 1], "*"))
     P_star<- numeritor/(1+numeritor)
-    
-    response[i, ] <- rowSums(P_star >= compare_sub[i])
+
+    response[i, ] <- rowSums(P_star >= runif(nrow(itempar), min=0, max=1))
   }
- 
-  
- return(response) 
-  
+
+
+  return(response)
+
 }
