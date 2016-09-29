81dce62999d1380cd2519aba0dc53498834258a3
Zhengguo Gu <z.gu@uvt.nl>
2016-09-29 16:19:13 +0200

    a simple simulation for graded response data

---
+++ simulation.R
@@ -0,0 +1,16 @@
+# data simulation
+
+################################################
+
+# simulate graded response data
+num_items <- 10
+num_persons <- 1000
+
+item_discri <- runif(num_items, min = 0.5, max = 1.5)
+item_diffi_avg <- runif(num_items, min = 0, max = 1.25)
+item_diff <- matrix(c(item_diffi_avg-2, item_diffi_avg-1, item_diffi_avg+1, item_diffi_avg+2),num_items, 4)
+itempar <- cbind(item_discri, item_diff)
+
+theta <- sort(rnorm(num_persons, 0, 1))
+
+response <- GRM_sim(theta, itempar)
