5ac2cf7bae27326aa24efb766db89bf860534907
Zhengguo Gu <z.gu@uvt.nl>
2016-09-29 18:28:03 +0200

    updated. but there are bugs there

--- GRM_sim.R
+++ GRM_sim.R
@@ -3,30 +3,20 @@
 GRM_sim <- function(ability, itempar){
   
   n_sub <- length(ability)
-  #P <- matrix(NA, nrow(itempar), ncol(itempar))
   response <- matrix(NA, n_sub, nrow(itempar))
   
+  compare_sub <- runif(n_sub, min=0, max=1)
+  
   for(i in 1:n_sub){
-    
-    P <- matrix(NA, nrow(itempar), ncol(itempar))
     
     
     numeritor <- exp(sweep((ability[i]-itempar[, -1]), 1, itempar[, 1], "*"))
     P_star<- numeritor/(1+numeritor)
     
-    P[, ncol(P_star)+1] <- P_star[,ncol(P_star)]
-    
-    for(k in ncol(P_star):2){
-      P[,k] <- P_star[,k-1] - P_star[,k]
-    }
-    
-    P[, 1] <- 1 - P_star[, 1]
-    
-    for(j in 1:nrow(itempar)) {
-      response[i, j] <- which(P[j,] == max(P[j,]))
-    }
+    response[i, ] <- rowSums(P_star >= compare_sub[i])
   }
  
+  
  return(response) 
   
 }
