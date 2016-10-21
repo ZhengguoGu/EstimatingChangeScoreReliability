03305fdb6935e543a673d4276b93815abcfe868e
Zhengguo Gu <z.gu@uvt.nl>
2016-10-21 17:48:08 +0200

    added a few commends for drawing plots for a fixed sample

--- simulation.R
+++ simulation.R
@@ -213,11 +213,54 @@ while(p<=maxp) {
 #
 ###################################################################################
 
+##########################################################
+# cross all samples of persons
 results <- r_avg
 
 plot(results[,1], results[, 2], xlab = "true reliability - proportion variance",
                      ylab = "true reliability - correlation", 
                      xlim=c(0, 1), ylim=c(0, 1),asp=1)
+lines(c(0,1),c(0,1),col="red")
+
+###
+
+plot(results[, 2], results[, 3], xlab = "true reliability - correlation",
+     ylab = "estimated reliability (pre and post reliability estimated by alpha )", 
+     xlim=c(0, 1), ylim=c(0, 1),asp=1)
+lines(c(0,1),c(0,1),col="red")
+
+###
+
+plot(results[, 2], results[, 4], xlab = "true reliability - correlation",
+     ylab = "estimated reliability (pre and post reliability estimated by lambda2)", 
+     xlim=c(0, 1), ylim=c(0, 1),asp=1)
+lines(c(0,1),c(0,1),col="red")
+
+###
+
+plot(results[, 2], results[, 6], xlab = "true reliability - correlation",
+     ylab = "estimated reliability (item-level) - alpha", 
+     xlim=c(0, 1), ylim=c(0, 1),asp=1)
+lines(c(0,1),c(0,1),col="red")
+
+###
+
+plot(results[, 2], results[, 7], xlab = "true reliability - correlation",
+     ylab = "estimated reliability (item-level) - lambda2", 
+     xlim=c(0, 1), ylim=c(0, 1),asp=1)
+lines(c(0,1),c(0,1),col="red")
+
+
+#######################
+# one sample 
+
+p <- sample(1:maxp, 1)
+
+results <- sample_results[[p]]
+
+plot(results[,1], results[, 2], xlab = "true reliability - proportion variance",
+     ylab = "true reliability - correlation", 
+     xlim=c(0, 1), ylim=c(0, 1),asp=1)
 lines(c(0,1),c(0,1),col="red")
 
 ###
