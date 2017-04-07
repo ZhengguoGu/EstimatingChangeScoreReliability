ae14ae5c5767e402dff94c4eae1532c71551fd9b
Zhengguo Gu <z.gu@uvt.nl>
2017-04-07 19:38:53 +0200

    Because i reorganize the Results section, I have a new R file for analyzing data. Note that the old R file is saved in this github R project.

---
+++ analyizingResultsNew20170407.R
@@ -0,0 +1,16 @@
+#########################################################################################################
+#
+# This file contains the code for analyzing the results
+#
+# I analyize the results in the following steps:
+# 1. Results for unidimensional data 
+#    1) No carry-over effects
+#    2) Carry-over effects
+# 2. Results for multidimensional data
+#    1) No carry-over effects
+#    2) Carry-over effects
+# 3. Design factors that influence the difference between change-score reliability and its estimate.
+#
+# Zhengguo Gu, Tilburg University
+# Last update: 07/04/2017
+#########################################################################################################
