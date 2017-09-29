9098ff3df8296fcc5efe0262a4d3405ca90ea142
Zhengguo Gu <z.gu@uvt.nl>
2017-09-29 11:52:56 +0200

    update

--- ZhengguoFunctions.R
+++ ZhengguoFunctions.R
@@ -73,6 +73,29 @@ GRM_sim <- function(ability, itempar, id){
   return(list(response, true_response))
 }
 
+#-----------------------------------------------------------------
+# GRM_sim_1theta: simulate response data based on the graded response
+# model (Samejima, 1969), given 1 theta
+#-----------------------------------------------------------------
+
+GRM_sim_1theta <- function(theta, itempar){
+  
+  # description:
+  #
+  # theta: person parameter; a scaler 
+  # itempar = item parameter
+  #
+  # This function is designed for items with more than 2 answer alternatives!
+  
+  true_response <- matrix(NA, 1, nrow(itempar))
+  
+  numeritor <- exp(itempar[, 1]*(theta - itempar[, -1]))
+  P_star <- numeritor/(1+numeritor) # this is the "true response"
+  P <- cbind(1, P_star)
+  P[, 1:4] <- P[, 1:4] - P_star    
+  return(P)  #returns the probability of answering each category 
+}
+
 
 #----------------------------------------------------------------
 # MulChange_sim: simulate multidimensional change in theta
@@ -145,3 +168,77 @@ carry_over <- function(pre, post){
   return(list(strong_post, weak_post))
 }
   
+
+#----------------------------------------------------------------------
+# Recursive algorithm for expanding Phi(X+|theta) for polytomous items,
+# which include GRM_sim_1theta, 
+#
+# Last update 2017-9-29
+#----------------------------------------------------------------------
+
+Phi_X <- function(P){
+  
+  # description:
+  # 
+  # P: the probabilities of answering each category per item correctly. 
+  #    It is a matrix, with rows representing items, and columns representing
+  #    answer categories.
+
+  # initial step:
+  p1 <- P[1, ]
+  p2 <- P[2, ]
+  cate_P <- 0:(dim(P)[2] - 1) #answer categories: 0, 1, 2, ....
+  
+  p_expand <- expand.grid(p1, p2)
+  p_expand <- p_expand[, 1] * p_expand[, 2]
+  cate_expand <- rowSums(expand.grid(cate_P, cate_P))
+  w <- matrix(NA, 1, max(cate_expand)+1)
+  for(i in 1:(max(cate_expand)+1)){
+    w[1, i] <- sum(p_expand[cate_expand == (i-1)])
+  }
+    
+  for(i in 3:dim(P)[1]){
+    p_i <- P[i, ]
+    p_expand <- expand.grid(p_i, w)
+    p_expand <- p_expand[, 1] * p_expand[, 2]
+    cate_expand <- rowSums(expand.grid(cate_P, 0:(length(w) - 1)))
+    w <- matrix(NA, 1, max(cate_expand)+1)
+    for(j in 1:(max(cate_expand)+1)){
+      w[1, j] <- sum(p_expand[cate_expand == (j-1)])
+    }
+  }
+  
+  return(w) #this is the probability of answering X+ = 0, 1, 2, 3, 4, ..... 
+}
+
+#----------------------------------------------------------------------
+# Calculat the full distribution of change scores. 
+#
+# Last update 2017-9-29
+#----------------------------------------------------------------------
+
+Phi_D <- function(Prob_pre, Prob_post){
+  
+  # Description
+  #
+  # Prob_pre: the probabilities of answering X+ = 0, 1, 2, ..., of a person at pretest
+  # Prob_post: the probabilities of answering X+ = 0, 1, 2, ..., of a person at posttest
+  
+  
+  cate_pre <- 0:(length(Prob_pre)-1)
+  cate_post <- 0:(length(Prob_post)-1)
+  
+  cate_expand <- expand.grid(cate_pre, cate_post)
+  D_scores <- cate_expand[, 2] - cate_expand[, 1]
+  prob_expand <- expand.grid(Prob_pre, Prob_post)
+  prob_D <- prob_expand[, 1] * prob_expand[, 2]
+  
+  prob_Dfinal <- matrix(NA, 1, length(unique(D_scores)))
+  D_index <- sort(unique(D_scores)) #change score sorted from min to max
+  for(j in 1:dim(prob_Dfinal)[2]){
+    prob_Dfinal[1, j] <- sum(prob_D[D_scores == D_index[j]]) # probabilities corresponding to D_index
+  }
+  return(D_index, prob_Dfinal)
+}
+
+
