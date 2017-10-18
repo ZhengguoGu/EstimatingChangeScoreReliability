570d12d27ba53c9ebda847020168e682b9d0ab2d
Zhengguo Gu <z.gu@uvt.nl>
2017-10-18 20:17:55 +0200

    update

--- ZhengguoFunctions.R
+++ ZhengguoFunctions.R
@@ -344,3 +344,51 @@ Qbipoints <- function(n_person, mu, sigma, bd){
   return(q_result)
   
 }
+
+#-----------------------------------------------------------------------------
+# Gauss-Hermite quadrature
+# The following code is obtained from https://biostatmatt.com/archives/2754
+#
+#------------------------------------------------------------------------------
+
+hermite <- function (points, z) {
+  p1 <- 1/pi^0.4
+  p2 <- 0
+  for (j in 1:points) {
+    p3 <- p2
+    p2 <- p1
+    p1 <- z * sqrt(2/j) * p2 - sqrt((j - 1)/j) * p3
+  }
+  pp <- sqrt(2 * points) * p2
+  c(p1, pp)
+}
+
+# compute multivariate Gaussian quadrature points
+## n     - number of points each dimension before pruning
+## mu    - mean vector
+## sigma - covariance matrix
+## prune - NULL - no pruning; [0-1] - fraction to prune
+mgauss.hermite <- function(n, mu, sigma, prune=NULL) {
+  if(!all(dim(sigma) == length(mu)))
+    stop("mu and sigma have nonconformable dimensions")
+  
+  dm  <- length(mu)
+  gh  <- gauss.hermite(n)
+  #idx grows exponentially in n and dm
+  idx <- as.matrix(expand.grid(rep(list(1:n),dm)))
+  pts <- matrix(gh[idx,1],nrow(idx),dm)
+  wts <- apply(matrix(gh[idx,2],nrow(idx),dm), 1, prod)
+  
+  ## prune
+  if(!is.null(prune)) {
+    qwt <- quantile(wts, probs=prune)
+    pts <- pts[wts > qwt,]
+    wts <- wts[wts > qwt]
+  }
+  
+  ## rotate, scale, translate points
+  eig <- eigen(sigma) 
+  rot <- eig$vectors %*% diag(sqrt(eig$values))
+  pts <- t(rot %*% t(pts) + mu)
+  return(list(points=pts, weights=wts))
+}
