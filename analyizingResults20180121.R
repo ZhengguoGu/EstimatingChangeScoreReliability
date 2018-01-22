45b1aa18f0a8b439cfb13cc3e77a2b9a29cf7dd3
Zhengguo Gu <z.gu@uvt.nl>
2018-01-22 22:29:37 +0100

    update

--- analyizingResults20180121.R
+++ analyizingResults20180121.R
@@ -189,11 +189,13 @@ Q2_Precision[situ1, ] #Unidimensional theta without carry-over effects --> P_uni
 write.csv(cbind(df[situ1, ], Q2_Precision[situ1, ]), file = "D:/Dropbox/Tilburg office/Research Individual change/Project 3 - item difference scores/20180121 DataAnalysis/P_uni_NoCarry_Large.csv")
 
 Q2_Precision[situ2, ] #Unidimensional theta with carry-over effects --> P_uni_Carry.csv
-write.csv(Q2_Precision[situ2, ], file = "D:/Dropbox/Tilburg office/Research Individual change/Project 3 - item difference scores/20180121 DataAnalysis/P_uni_Carry_Large.csv")
+write.csv(cbind(df[situ2, ], Q2_Precision[situ2, ]), file = "D:/Dropbox/Tilburg office/Research Individual change/Project 3 - item difference scores/20180121 DataAnalysis/P_uni_Carry_Large.csv")
 
+Q2_Precision[situ3, ] #Multidimensional theta without carry-over effects --> P_mul_NoCarry.csv
+write.csv(cbind(df[situ3, ], Q2_Precision[situ3, ]), file = "D:/Dropbox/Tilburg office/Research Individual change/Project 3 - item difference scores/20180121 DataAnalysis/P_mul_NoCarry_Large.csv")
 
-write.csv(Q2_Precision[situ3, ], file = "D:/Dropbox/Tilburg office/Research Individual change/Project 3 - item difference scores/20180121 DataAnalysis/situ3_LargeSample.csv")
-write.csv(Q2_Precision[situ4, ], file = "D:/Dropbox/Tilburg office/Research Individual change/Project 3 - item difference scores/20180121 DataAnalysis/situ4_LargeSample.csv")
+Q2_Precision[situ4, ] #Multidimensional theta with carry-over effects --> P_mul_Carry.csv
+write.csv(cbind(df[situ4, ], Q2_Precision[situ4, ]), file = "D:/Dropbox/Tilburg office/Research Individual change/Project 3 - item difference scores/20180121 DataAnalysis/P_mul_Carry_Large.csv")
 
 
 ############# 4.  given the 108 cells, which cells generate negative reliabilites? ####################
